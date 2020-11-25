7999bc0303c623f14a6cd8901cd9ecf9111d8360
mtKeller <micah.t.keller@gmail.com>
2020-11-25 14:14:13 -0800

    Initial structure created, now flesh out child process.

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,34 @@ mod kinder {
     // use tokio::time;
     // use tokio::process::{Child, Command};
     use tokio::io::{AsyncBufReadExt, AsyncWriteExt, AsyncWrite, AsyncRead, BufReader};
+    use std::error::Error;
+    use std::fmt;
     // use std::io::{ self, Write, Read };
     // use std::process::{ChildStdin, ChildStdout, Stdio};
     // use futures::*;
     // use std::sync::mpsc::channel;
     // use std::thread;
     pub struct Kind {
-        
+        pub path: String,
+    }
+
+    impl Default for Kind {
+        fn default() -> Self { Self { path: String::from("Hello World") } }
+    }
+
+    impl fmt::Debug for Kind {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "Path: {}", self.path)
+        }
+    }
+
+    impl Kind {
+        async fn dispatch(&self) {}
+        async fn subscribe(&self) {}
+    }
+
+    pub fn kind () -> Result<Kind, String> {
+        Ok(Kind::default())
     }
 
     pub async fn kind_write<W: AsyncWrite + Unpin, U: Unpin>(
@@ -22,13 +43,24 @@ mod kinder {
         kind_in.write_all(str.as_bytes()).await.unwrap();
     }
 
-    pub async fn kind_read<R: AsyncRead>(
-        kind_out: R,
+    // pub async fn kind_read<R: AsyncRead>(
+    //     kind_out: R,
 
-    ) -> String {
-        let mut reader = BufReader::new(kind_out).lines();
+    // ) -> String {
+    //     let mut reader = BufReader::new(kind_out).lines();
 
-        let data = reader.next_line().await;
-        String::from("TEST")
+    //     let data = reader.next_line().await;
+    //     String::from("TEST")
+    // }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::kinder::*;
+    #[test]
+    fn creates() {
+        let kind = kind().unwrap();
+
+        assert_eq!(kind.path, "Hello World");
     }
 }
